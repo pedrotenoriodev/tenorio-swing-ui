87ec8a406190030c3ba72335015c91aa51cc4efa
Pedro Tenório <pedrohenriquetenorio@gmail.com>
2026-05-30 08:57:58 -0300

    primeiro comit

---
+++ src/main/java/br/com/tenorio/swing/ui/TenorioSwingUi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.tenorio.swing.ui;
+
+/**
+ *
+ * @author Pedro
+ */
+public class TenorioSwingUi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
